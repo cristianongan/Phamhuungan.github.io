29ead62fd48fe8b39b5a0d91fba69133544d6ca4
LinhLH <linh.lehong@evotek.vn>
2021-04-22 11:07:31 +0700

    Create new project

---
+++ src/main/java/com/evotek/qlns/QlnsZkApplication.java
@@ -0,0 +1,13 @@
+package com.evotek.qlns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlnsZkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlnsZkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evotek/qlns/QlnsZkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evotek.qlns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlnsZkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
